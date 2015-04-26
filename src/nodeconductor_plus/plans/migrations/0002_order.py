4f1206f9270002cf48d577ed668da656d632d35b
Pavel <marchukpavelp@gmail.com>
2015-04-26 14:38:01 +0400

    Create order model (saas-58)

---
+++ src/nodeconductor_plus/plans/migrations/0002_order.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import uuidfield.fields
+import django_fsm
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('structure', '0006_inherit_namemixin'),
+        ('plans', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Order',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('uuid', uuidfield.fields.UUIDField(unique=True, max_length=32, editable=False, blank=True)),
+                ('customer_name', models.CharField(max_length=150)),
+                ('plan_name', models.CharField(max_length=120)),
+                ('plan_price', models.DecimalField(max_digits=12, decimal_places=2)),
+                ('state', django_fsm.FSMField(default=b'processing', help_text=b'WARNING! Should not be changed manually unless you really know what you are doing.', max_length=20, choices=[(b'processing', b'Processing'), (b'failed', b'Failed'), (b'completed', b'Completed'), (b'erred', b'Erred')])),
+                ('customer', models.ForeignKey(to='structure.Customer', null=True)),
+                ('plan', models.ForeignKey(related_name='orders', to='plans.Plan', null=True)),
+            ],
+            options={
+                'abstract': False,
+            },
+            bases=(models.Model,),
+        ),
+    ]
