9f53cb047665bdb57f088f872f39320bcad1cb3b
Pavel <marchukpavelp@gmail.com>
2015-05-14 16:27:35 +0300

    Update full name on login (saas-325)

--- src/nodeconductor_plus/nodeconductor_auth/views.py
+++ src/nodeconductor_plus/nodeconductor_auth/views.py
@@ -60,10 +60,13 @@ class GoogleView(views.APIView):
             return response.Response(
                 {'message': response_data['error']['message']}, status=response_data['error']['code'])
 
-        # Step 3. Create a new user or get existing one.
+        # Step 4. Create a new user or get existing one.
         try:
             profile = AuthProfile.objects.get(google=response_data['sub'])
             token, _ = Token.objects.get_or_create(user=profile.user)
+            if profile.user.full_name != response_data['name']:
+                profile.user.full_name = response_data['name']
+                profile.user.save()
             return response.Response({'token': token.key}, status=status.HTTP_200_OK)
         except AuthProfile.DoesNotExist:
             user = get_user_model().objects.create_user(
@@ -99,17 +102,23 @@ class FacebookView(views.APIView):
 
         # Step 2. Retrieve information about the current user.
         r = requests.get(graph_api_url, params=access_token)
-        profile = json.loads(r.text)
+        response_data = json.loads(r.text)
 
         # Step 3. Create a new user or get existing one.
         try:
-            profile = AuthProfile.objects.get(facebook=profile['id'])
+            profile = AuthProfile.objects.get(facebook=response_data['id'])
             token, _ = Token.objects.get_or_create(user=profile.user)
+            if profile.user.full_name != response_data['name']:
+                profile.user.full_name = response_data['name']
+                profile.user.save()
             return response.Response({'token': token.key}, status=status.HTTP_200_OK)
         except AuthProfile.DoesNotExist:
             user = get_user_model().objects.create_user(
-                username=generate_username(profile['name']), password=generate_password(), full_name=profile['name'])
-            user.auth_profile.facebook = profile['id']
+                username=generate_username(response_data['name']),
+                password=generate_password(),
+                full_name=response_data['name']
+            )
+            user.auth_profile.facebook = response_data['id']
             user.auth_profile.save()
             token, _ = Token.objects.get_or_create(user=user)
             return response.Response({'token': token.key}, status=status.HTTP_201_CREATED)
