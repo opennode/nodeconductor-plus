526304a09dee6d421d3698aa012d1832e4c5eddc
Pavel <marchukpavelp@gmail.com>
2015-05-14 16:30:43 +0300

    Update customer on user full_name change(saas-325)

--- src/nodeconductor_plus/nodeconductor_auth/handlers.py
+++ src/nodeconductor_plus/nodeconductor_auth/handlers.py
@@ -1,3 +1,5 @@
+from django.contrib.auth import get_user_model
+
 from nodeconductor.structure import models as structure_models
 from .models import AuthProfile
 
@@ -16,3 +18,13 @@ def create_user_first_customer_and_project(sender, instance=None, created=False,
     customer.add_user(user, structure_models.CustomerRole.OWNER)
     project = structure_models.Project.objects.create(customer=customer, name='My First Project')
     project.add_user(user, structure_models.ProjectRole.ADMINISTRATOR)
+
+
+def update_user_first_customer_name_on_user_name_change(sender, instance=None, created=False, **kwargs):
+    if instance.id is None:
+        return
+
+    new_user = instance
+    old_user = get_user_model().objects.get(id=instance.id)
+    if new_user.full_name != old_user.full_name:
+        structure_models.Customer.objects.filter(name=old_user.full_name).update(name=new_user.full_name)
--- src/nodeconductor_plus/nodeconductor_auth/tests/unittests/test_signals.py
+++ src/nodeconductor_plus/nodeconductor_auth/tests/unittests/test_signals.py
@@ -7,9 +7,16 @@ from nodeconductor.structure import models as structure_models
 class SignalsTest(TransactionTestCase):
 
     def test_first_customer_and_project_creation_on_user_creation(self):
-        user = get_user_model().objects.create_user(username='test', password='test')
+        user = get_user_model().objects.create_user(username='test', password='test', full_name='test_user')
 
-        customer = structure_models.Customer.objects.get(name=user.username)
-        project = structure_models.Project.objects.get(name='My First Project', customer__name=user.username)
+        customer = structure_models.Customer.objects.get(name=user.full_name)
+        project = structure_models.Project.objects.get(name='My First Project', customer__name=user.full_name)
         self.assertTrue(customer.has_user(user, structure_models.CustomerRole.OWNER))
         self.assertTrue(project.has_user(user, structure_models.ProjectRole.ADMINISTRATOR))
+
+    def test_first_customer_name_update_on_user_full_name_update(self):
+        user = get_user_model().objects.create_user(username='test', password='test', full_name='test_user')
+        self.assertTrue(structure_models.Customer.objects.filter(name=user.full_name).exists())
+        user.full_name = 'new_full_name'
+        user.save()
+        self.assertTrue(structure_models.Customer.objects.filter(name=user.full_name).exists())
