8630335f10c0d3b7486cd789c3ec2c1ed21faf3e
Victor Mireyev <victor@opennodecloud.com>
2015-07-07 18:25:46 +0300

    Split premium support tests (SAAS-486)

---
+++ src/nodeconductor_plus/premium_support/tests/test_support_plan.py
@@ -0,0 +1,44 @@
+from rest_framework import status, test
+
+from nodeconductor.structure.tests import factories as structure_factories
+from nodeconductor_plus.premium_support.tests import factories as support_factories
+
+class SupportPlanTest(test.APITransactionTestCase):
+    def setUp(self):
+        self.staff = structure_factories.UserFactory(is_staff=True)
+        self.user = structure_factories.UserFactory()
+        self.plans = [
+            {
+                'name': 'Bronze support',
+                'description': 'Gives you access to online documentation, community forums, and billing support.',
+                'base_hours': 100,
+                'hour_rate': 10
+            },
+            {
+                'name': 'Silver support',
+                'description': 'Gives you direct access to our support team.',
+                'base_hours': 200,
+                'hour_rate': 20
+            },
+            {
+                'name': 'Gold support',
+                'description': '24x7 phone support, rapid response times and consultation on application development.',
+                'base_hours': 300,
+                'hour_rate': 30
+            }
+        ]
+
+    def test_staff_can_create_plan(self):
+        self.client.force_authenticate(self.staff)
+        for plan in self.plans:
+            self.create_plan(plan)
+
+    def test_user_can_get_plan_by_uuid(self):
+        plan = support_factories.PlanFactory()
+        self.client.force_authenticate(self.user)
+        response = self.client.get(support_factories.PlanFactory.get_url(plan))
+        self.assertEqual(response.status_code, status.HTTP_200_OK, response.data)
+
+    def create_plan(self, plan):
+        response = self.client.post(support_factories.PlanFactory.get_list_url(), data=plan)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED, response.data)
