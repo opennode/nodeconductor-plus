67b636be122dc05bc673989903c4d503c7ea694a
Pavel <marchukpavelp@gmail.com>
2015-08-31 13:52:29 +0400

    Allow owner to create resources
     - saas-629

--- src/nodeconductor_plus/aws/perms.py
+++ src/nodeconductor_plus/aws/perms.py
@@ -1,5 +1,5 @@
 from nodeconductor.core.permissions import FilteredCollaboratorsPermissionLogic, StaffPermissionLogic
-from nodeconductor.structure import models as structure_models
+from nodeconductor.structure import models as structure_models, perms as structure_perms
 
 
 PERMISSION_LOGICS = (
@@ -22,13 +22,6 @@ PERMISSION_LOGICS = (
 
         any_permission=True,
     )),
-    ('aws.Instance', FilteredCollaboratorsPermissionLogic(
-        collaborators_query='service_project_link__project__roles__permission_group__user',
-        collaborators_filter={
-            'service_project_link__project__roles__role_type': structure_models.ProjectRole.ADMINISTRATOR,
-        },
-
-        any_permission=True,
-    )),
+    ('aws.Instance', structure_perms.resource_permission_logic),
     ('aws.Image', StaffPermissionLogic(any_permission=True)),
 )
