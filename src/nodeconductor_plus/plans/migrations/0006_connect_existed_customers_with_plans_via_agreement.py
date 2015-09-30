01d562f6a3bce5a06b7496e081d69cab41948e9e
Dmitri Tsumak <tsumak.dmitri@gmail.com>
2015-09-30 13:33:07 +0300

    Fix default plan creation
    NC-829

--- src/nodeconductor_plus/plans/migrations/0006_connect_existed_customers_with_plans_via_agreement.py
+++ src/nodeconductor_plus/plans/migrations/0006_connect_existed_customers_with_plans_via_agreement.py
@@ -19,16 +19,16 @@ def connect_existed_customers_with_default_plan(apps, schema_editor):
     PlanQuota = apps.get_model('plans', 'PlanQuota')
     Agreement = apps.get_model('plans', 'Agreement')
 
-    default_plan, created = Plan.objects.get_or_create(
-        uuid=uuid4().hex, name=DEFAULT_PLAN['name'], price=DEFAULT_PLAN['price'])
-    if created:
+    if not Plan.objects.filter(name=DEFAULT_PLAN['name'], price=DEFAULT_PLAN['price']).exists():
+        default_plan = Plan.objects.create(
+            uuid=uuid4().hex, name=DEFAULT_PLAN['name'], price=DEFAULT_PLAN['price'])
         for quota_name, quota_value in DEFAULT_PLAN['quotas']:
             PlanQuota.objects.get_or_create(name=quota_name, value=quota_value, plan=default_plan)
 
-    for customer in Customer.objects.all():
-        if not Agreement.objects.filter(customer=customer).exists():
-            # We need to add UUID explicitly, because django ignores auto=True parameter in migration UUID field
-            Agreement.objects.create(uuid=uuid4().hex, customer=customer, plan=default_plan, state='active')
+        for customer in Customer.objects.all():
+            if not Agreement.objects.filter(customer=customer).exists():
+                # We need to add UUID explicitly, because django ignores auto=True parameter in migration UUID field
+                Agreement.objects.create(uuid=uuid4().hex, customer=customer, plan=default_plan, state='active')
 
 
 class Migration(migrations.Migration):
