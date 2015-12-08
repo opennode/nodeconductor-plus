d3186e7286474849be13b12d1a7197c244d7cdf9
Roman Kosenko <Roman.Kosenko@gmail.com>
2015-12-08 09:16:47 +0000

    Default price lists for cloud services
    - NC-967

--- src/nodeconductor_plus/digitalocean/cost_tracking.py
+++ src/nodeconductor_plus/digitalocean/cost_tracking.py
@@ -1,8 +1,36 @@
-from . import models
+from django.contrib.contenttypes.models import ContentType
+
 from nodeconductor.cost_tracking import CostTrackingBackend
+from nodeconductor.cost_tracking.models import DefaultPriceListItem
+
+from . import models
 
 
 class DigitalOceanCostTrackingBackend(CostTrackingBackend):
+
+    @classmethod
+    def get_default_price_list_items(cls):
+        ct = ContentType.objects.get_for_model(models.Droplet)
+        # XXX: hardcode a list of sizes with prices since it's available only via API
+        sizes = {
+            '1gb': 10.0,
+            '2gb': 20.0,
+            '4gb': 40.0,
+            '8gb': 80.0,
+            '16gb': 160.0,
+            '32gb': 320.0,
+            '48gb': 480.0,
+            '64gb': 640.0,
+            '512mb': 5.0,
+        }
+
+        # sizes
+        for name, price in sizes.items():
+            yield DefaultPriceListItem(
+                resource_content_type=ct,
+                item_type='size',
+                key=name,
+                value=price)
 
     @classmethod
     def get_monthly_cost_estimate(cls, resource):
