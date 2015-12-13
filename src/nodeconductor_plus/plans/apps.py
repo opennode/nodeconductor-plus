4f13c4d1d4d1af5189fad3c20565c4770fefbf21
Pavel <marchukpavelp@gmail.com>
2015-12-13 12:30:58 +0400

    Correct imports
     - nc-993

--- src/nodeconductor_plus/plans/apps.py
+++ src/nodeconductor_plus/plans/apps.py
@@ -3,15 +3,14 @@ from __future__ import unicode_literals
 from django.apps import AppConfig
 from django.db.models import signals
 
-from nodeconductor.structure import models as structure_models
 from . import handlers
+from nodeconductor.structure import models as structure_models
 
 
 class PlansConfig(AppConfig):
     name = 'nodeconductor_plus.plans'
     verbose_name = 'NodeConductorPlus Plans'
 
-    # See, https://docs.djangoproject.com/en/1.7/ref/applications/#django.apps.AppConfig.ready
     def ready(self):
         signals.post_save.connect(
             handlers.create_customer_plan,
