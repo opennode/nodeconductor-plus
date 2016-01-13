1aacda7aae63214c016bc17a14bf321f7b3d1dab
Victor Mireyev <victor@opennodecloud.com>
2016-01-13 13:15:39 +0300

    Add ability to expose location with coordinates to VMs (SAAS-1086)

--- src/nodeconductor_plus/aws/admin.py
+++ src/nodeconductor_plus/aws/admin.py
@@ -4,6 +4,6 @@ from nodeconductor.structure import admin as structure_admin
 from .models import AWSService, AWSServiceProjectLink, Instance
 
 
-admin.site.register(Instance, structure_admin.ResourceAdmin)
+admin.site.register(Instance, structure_admin.VirtualMachineAdmin)
 admin.site.register(AWSService, structure_admin.ServiceAdmin)
 admin.site.register(AWSServiceProjectLink, structure_admin.ServiceProjectLinkAdmin)
