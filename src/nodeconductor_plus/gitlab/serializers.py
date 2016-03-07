ed52494c5eb9bc6661a29cfbb0c267a53ff9136a
Dmitri Tsumak <tsumak.dmitri@gmail.com>
2016-03-07 11:19:25 +0200

    Add settings check on path and name validation
    - SAAS-1185

--- src/nodeconductor_plus/gitlab/serializers.py
+++ src/nodeconductor_plus/gitlab/serializers.py
@@ -87,11 +87,6 @@ class GroupSerializer(structure_serializers.BaseResourceSerializer):
             raise serializers.ValidationError(
                 {'name': "Name can only contain letters, digits, '_', '.', dash and space."})
 
-        if models.Group.objects.filter(name=attrs['name']).exists():
-            raise serializers.ValidationError(
-                {'name': "Group with this name already exists."}
-            )
-
         if not re.match(r'^[a-zA-Z0-9_.\s-]+$', attrs['path']):
             raise serializers.ValidationError(
                 {'path': "Path can only contain letters, digits, '_', '.', dash and space."})
@@ -100,7 +95,24 @@ class GroupSerializer(structure_serializers.BaseResourceSerializer):
             raise serializers.ValidationError(
                 {'path': "Path cannot start with '-' or end in '.'."})
 
-        if models.Group.objects.filter(path=attrs['path']).exists():
+        spl = attrs['service_project_link']
+
+        # Name should be unique on backend
+        if models.Group.objects.filter(
+                name=attrs['name'],
+                service_project_link__service__settings__backend_url=spl.service.settings.backend_url,
+                service_project_link__service__settings__username=spl.service.settings.username
+        ).exists():
+            raise serializers.ValidationError(
+                {'name': "Group with this name already exists."}
+            )
+
+        # Path should be unique on backend
+        if models.Group.objects.filter(
+                path=attrs['path'],
+                service_project_link__service__settings__backend_url=spl.service.settings.backend_url,
+                service_project_link__service__settings__username=spl.service.settings.username
+        ).exists():
             raise serializers.ValidationError(
                 {'path': "Group with this path already exists."})
 
