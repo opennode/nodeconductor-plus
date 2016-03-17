3ad1173a59f9cc47b81c69a55468dd871e09e809
Roman Kosenko <Roman.Kosenko@gmail.com>
2016-03-17 10:03:00 +0000

    Update AWS images instead of complete sync
    - SAAS-1069

--- src/nodeconductor_plus/aws/admin.py
+++ src/nodeconductor_plus/aws/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 
 from nodeconductor.structure import admin as structure_admin
-from .models import AWSService, AWSServiceProjectLink, Instance
+from .models import AWSService, AWSServiceProjectLink, Instance, Image
 
 
+class ImageAdmin(admin.ModelAdmin):
+    fields = 'name', 'region', 'backend_id'
+    list_display = 'name', 'region', 'backend_id'
+    list_filter = 'region',
+
+
+admin.site.register(Image, ImageAdmin)
 admin.site.register(Instance, structure_admin.VirtualMachineAdmin)
 admin.site.register(AWSService, structure_admin.ServiceAdmin)
 admin.site.register(AWSServiceProjectLink, structure_admin.ServiceProjectLinkAdmin)
