86fdd8d4b02bdb76730cb1d3c1b5c7167e0ce8ac
Victor Mireyev <victor@opennodecloud.com>
2016-04-01 12:27:20 +0300

    Make permissions declaration DRY (NC-1282)

--- src/nodeconductor_plus/azure/perms.py
+++ src/nodeconductor_plus/azure/perms.py
@@ -1,27 +1,10 @@
-from nodeconductor.core.permissions import FilteredCollaboratorsPermissionLogic, StaffPermissionLogic
-from nodeconductor.structure import models as structure_models, perms as structure_perms
+from nodeconductor.core.permissions import StaffPermissionLogic
+from nodeconductor.structure import perms as structure_perms
 
 
 PERMISSION_LOGICS = (
-    ('azure.AzureService', FilteredCollaboratorsPermissionLogic(
-        collaborators_query='customer__roles__permission_group__user',
-        collaborators_filter={
-            'customer__roles__role_type': structure_models.CustomerRole.OWNER,
-        },
-        any_permission=True,
-    )),
-    ('azure.AzureServiceProjectLink', FilteredCollaboratorsPermissionLogic(
-        collaborators_query=[
-            'service__customer__roles__permission_group__user',
-            'project__project_groups__roles__permission_group__user',
-        ],
-        collaborators_filter=[
-            {'service__customer__roles__role_type': structure_models.CustomerRole.OWNER},
-            {'project__project_groups__roles__role_type': structure_models.ProjectGroupRole.MANAGER},
-        ],
-
-        any_permission=True,
-    )),
+    ('azure.AzureService', structure_perms.service_permission_logic),
+    ('azure.AzureServiceProjectLink', structure_perms.service_project_link_permission_logic),
     ('azure.VirtualMachine', structure_perms.resource_permission_logic),
     ('azure.Image', StaffPermissionLogic(any_permission=True)),
 )
--- src/nodeconductor_plus/digitalocean/perms.py
+++ src/nodeconductor_plus/digitalocean/perms.py
@@ -1,27 +1,10 @@
-from nodeconductor.core.permissions import FilteredCollaboratorsPermissionLogic, StaffPermissionLogic
-from nodeconductor.structure import models as structure_models, perms as structure_perms
+from nodeconductor.core.permissions import StaffPermissionLogic
+from nodeconductor.structure import perms as structure_perms
 
 
 PERMISSION_LOGICS = (
-    ('digitalocean.DigitalOceanService', FilteredCollaboratorsPermissionLogic(
-        collaborators_query='customer__roles__permission_group__user',
-        collaborators_filter={
-            'customer__roles__role_type': structure_models.CustomerRole.OWNER,
-        },
-        any_permission=True,
-    )),
-    ('digitalocean.DigitalOceanServiceProjectLink', FilteredCollaboratorsPermissionLogic(
-        collaborators_query=[
-            'service__customer__roles__permission_group__user',
-            'project__project_groups__roles__permission_group__user',
-        ],
-        collaborators_filter=[
-            {'service__customer__roles__role_type': structure_models.CustomerRole.OWNER},
-            {'project__project_groups__roles__role_type': structure_models.ProjectGroupRole.MANAGER},
-        ],
-
-        any_permission=True,
-    )),
+    ('digitalocean.DigitalOceanService', structure_perms.service_permission_logic),
+    ('digitalocean.DigitalOceanServiceProjectLink', structure_perms.service_project_link_permission_logic),
     ('digitalocean.Droplet', structure_perms.resource_permission_logic),
     ('digitalocean.Image', StaffPermissionLogic(any_permission=True)),
     ('digitalocean.Region', StaffPermissionLogic(any_permission=True)),
