c4f9340bf78271f08a1ce50830164638234d2cfb
Victor Mireyev <victor@opennodecloud.com>
2016-05-12 12:37:03 +0300

    Refresh auth token on successfull Facebook login (NC-1386)

--- src/nodeconductor_plus/nodeconductor_auth/tests/test_auth.py
+++ src/nodeconductor_plus/nodeconductor_auth/tests/test_auth.py
@@ -1,4 +1,6 @@
 import mock
+from django.conf import settings
+from django.utils import timezone
 
 from rest_framework import status, test
 from rest_framework.reverse import reverse
@@ -27,37 +29,49 @@ class AuthTest(test.APITransactionTestCase):
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_if_google_auth_succeeded_user_and_profile_is_created(self):
-        with mock.patch('nodeconductor_plus.nodeconductor_auth.'
-                        'views.GoogleView.get_backend_user') as get_backend_user:
-            get_backend_user.return_value = {
-                'id': '123',
-                'name': 'Google user'
-            }
-            response = self.client.post(reverse('auth_google'), self.valid_data)
-            self.assertEqual(response.status_code, status.HTTP_201_CREATED, response.data)
-            self.assertEqual('Google user', AuthProfile.objects.get(google='123').user.full_name)
+        response = self.google_login()
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED, response.data)
+        self.assertEqual('Google user', AuthProfile.objects.get(google='123').user.full_name)
 
     def test_if_user_already_exists_it_is_not_created_again(self):
         user = structure_factories.UserFactory()
         user.auth_profile.google = '123'
         user.auth_profile.save()
 
+        response = self.google_login()
+        self.assertEqual(response.status_code, status.HTTP_200_OK, response.data)
+
+    def test_if_facebook_auth_succeeded_user_and_profile_is_created(self):
+        response = self.facebook_login()
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED, response.data)
+        self.assertEqual('Facebook user', AuthProfile.objects.get(facebook='123').user.full_name)
+
+    def test_expired_token_is_recreated_on_successful_authentication(self):
+        response = self.google_login()
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        token1 = response.data['token']
+
+        lifetime = settings.NODECONDUCTOR.get('TOKEN_LIFETIME', timezone.timedelta(hours=1))
+        mocked_now = timezone.now() + lifetime
+        with mock.patch('django.utils.timezone.now', lambda: mocked_now):
+            response = self.google_login()
+            token2 = response.data['token']
+            self.assertNotEqual(token1, token2)
+
+    def google_login(self):
         with mock.patch('nodeconductor_plus.nodeconductor_auth.'
                         'views.GoogleView.get_backend_user') as get_backend_user:
             get_backend_user.return_value = {
                 'id': '123',
                 'name': 'Google user'
             }
-            response = self.client.post(reverse('auth_google'), self.valid_data)
-            self.assertEqual(response.status_code, status.HTTP_200_OK, response.data)
+            return self.client.post(reverse('auth_google'), self.valid_data)
 
-    def test_if_facebook_auth_succeeded_user_and_profile_is_created(self):
+    def facebook_login(self):
         with mock.patch('nodeconductor_plus.nodeconductor_auth.'
                         'views.FacebookView.get_backend_user') as get_backend_user:
             get_backend_user.return_value = {
                 'id': '123',
                 'name': 'Facebook user'
             }
-            response = self.client.post(reverse('auth_facebook'), self.valid_data)
-            self.assertEqual(response.status_code, status.HTTP_201_CREATED, response.data)
-            self.assertEqual('Facebook user', AuthProfile.objects.get(facebook='123').user.full_name)
+            return self.client.post(reverse('auth_facebook'), self.valid_data)
