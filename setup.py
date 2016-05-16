a85c228ff5283bbdd89f0944961ebe4bdc8d06d5
Roman Kosenko <Roman.Kosenko@gmail.com>
2016-05-16 07:57:46 +0000

    Fix typo

--- setup.py
+++ setup.py
@@ -19,8 +19,8 @@ tests_require = [
 install_requires = [
     'apache-libcloud>=0.20.0',
     # Consider moving nodeconductor_plus.plans to nodeconductor_paypal
-    'nodeconductor_openstack>=0.1.0',
     'nodeconductor_paypal>=0.3.0',
+    'nodeconductor>0.91.0',
     'python-digitalocean>=1.5',
     'python-gitlab>=0.9',
 ]
