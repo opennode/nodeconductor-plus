2d4fbf96f4bd9e0f35776c4b4c7a3eab71a3033e
Dmitri Tsumak <tsumak.dmitri@gmail.com>
2016-11-11 19:01:33 +0200

    Update ncplus version
    - WAL-108

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ tests_require = [
 
 install_requires = [
     'apache-libcloud>=1.1.0',
-    'nodeconductor>0.108.3',
+    'nodeconductor>0.109.0',
     'html5lib<0.99999999',  # Transient dependency for nodeconductor_paypal
     # Consider moving nodeconductor_plus.plans to nodeconductor_paypal
     'nodeconductor_paypal>0.3.5',
